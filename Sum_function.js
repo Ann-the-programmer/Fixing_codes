8b639594ed851720f6d6a32bcebf2ba7ad6aa5c4
Ann Ramazanova <94097590+Ann-the-programmer@users.noreply.github.com>
2021-12-03 03:35:39 +0100

    Create Sum_function.js
    JavaScript function for summing any number of arguments the user enters (using "for..of" loop)

---
+++ Sum_function.js
@@ -0,0 +1,11 @@
+function summingArguments(numbers) {
+    let sumOfArgs = 0;
+    for (let i of numbers) {
+        sumOfArgs += parseInt(i);
+    }
+    return sumOfArgs;
+}
+
+let _userInput = prompt("Please, enter some numbers separated with commas&spaces (e.g. '13, 15, 17, 23')");
+let $modifiedUserInput = _userInput.split(", ");
+console.log((summingArguments($modifiedUserInput)));
